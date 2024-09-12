6bc6cee49f9ae8423f64bbefbfaa8800170a3b73
SargamPuram <sargampuram3@gmail.com>
2024-09-12 16:26:17 +0530

    Added the basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Select a Platform 
+      </header>
+      <div className="flex justify-between items-center">
+        <div>
+          <ol>
+            <li>WhatsApp</li>
+            <li>Instagram</li>
+
+          </ol>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
